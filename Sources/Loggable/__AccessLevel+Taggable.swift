195f90276e01097cc5831690cb7f7a71561e8d75
23122K <hazy.parader-0r@icloud.com>
2025-06-04 22:47:31 +0200

    WIP

--- Sources/Loggable/__AccessLevel+Taggable.swift
+++ Sources/Loggable/__AccessLevel+Taggable.swift
@@ -1,25 +1,46 @@
-public enum __AccessLevel: Taggable {
+/// An enumeration that defines access levels.
+///
+/// @Comment {
+///   See https://docs.swift.org/swift-book/documentation/the-swift-programming-language/accesscontrol/
+///   for additional information.
+/// }
+public enum __AccessLevelModifier: Taggable {
+  /// A string literal representation of access level keyword.
   case tag(StringLiteralType)
   
+  /// Initialize an instance of this type that represents Swift access level.
+  ///
+  /// Currently, `__AccessLevelModifier` is only extracted
+  /// as a `MemberAccessExprSyntax`.
+  /// When declaring a new instance, ensure that both the tag’s associated value
+  /// and its property name matches matches the keyword.
+  ///
+  /// - Parameter value: A string literal that must match a Swift access level
+  ///   keyword (e.g. "public", "private").
+  @_spi(Experimental)
   public init(stringLiteral value: StringLiteralType) {
-    self = __AccessLevel.tag(value)
+    self = __AccessLevelModifier.tag(value)
   }
 }
 
-extension Taggable where Self == __AccessLevel {
-  public static var `public`: __AccessLevel {
-    __AccessLevel(stringLiteral: "public")
+extension Taggable where Self == __AccessLevelModifier {
+  /// A `public` access level modifier.
+  public static var `public`: __AccessLevelModifier {
+    __AccessLevelModifier(stringLiteral: "public")
   }
   
-  public static var `internal`: __AccessLevel {
-    __AccessLevel(stringLiteral: "internal")
+  /// An `internal` access level modifier.
+  public static var `internal`: __AccessLevelModifier {
+    __AccessLevelModifier(stringLiteral: "internal")
   }
   
-  public static var `fileprivate`: __AccessLevel {
-    __AccessLevel(stringLiteral: "fileprivate")
+  /// A `fileprivate` access level modifier.
+  public static var `fileprivate`: __AccessLevelModifier {
+    __AccessLevelModifier(stringLiteral: "fileprivate")
   }
   
-  public static var `private`: __AccessLevel {
-    __AccessLevel(stringLiteral: "private")
+  /// A `private` access level modifier.
+  public static var `private`: __AccessLevelModifier {
+    __AccessLevelModifier(stringLiteral: "private")
   }
 }
